63ecf415e6b096f20b12135c3acf4fe41a877d41
richfig683 <richfig683@gmail.com>
2021-04-03 12:08:34 -0400

    fix the main layout path not recoginised so set it in the event subscriber

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -29,6 +29,7 @@ export class MainLayoutComponent implements OnInit {
     var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
     this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
       console.log('event');
+      this.path = this.router.url;
       if (window.outerWidth > 991) {
           window.document.children[0].scrollTop = 0;
       }else{
